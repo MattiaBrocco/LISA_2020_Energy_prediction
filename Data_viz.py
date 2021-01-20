589a0e114f2ffd076592c401b8077c158be9faa3
mattiabrocco <61026948+MattiaBrocco@users.noreply.github.com>
2021-01-20 21:34:56 +0100

    Code files - upload 2021/01/20

---
+++ Data_viz.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+
+class Graphs:
+    
+    def plot_1(self, df):
+        self.df = df
+    
+        # Set up the plotting layout
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows = 2, ncols = 2, figsize = (35, 15))
+        fig.autofmt_xdate(rotation = 45)
+
+        # TEMPERATURE
+
+        # Living Room 
+        ax1.plot(df.date, df.living_celsius, linewidth = 1, alpha = 0.7, label = "Living Room")
+        ax1.set_xlabel('Nov 1st 2016 - May 27th 2016'); ax1.set_ylabel('Temp in Celsius'); ax1.set_title('Temperature Trend')
+        # Parents Room 
+        ax1.plot(df.date, df.parents_celsius, linewidth = 1, alpha = 0.7, label = "Parents Room")
+        ax1.set_xlabel('Nov 1st 2016 - May 27th 2016');
+        # Ironing Room 
+        ax1.plot(df.date, df.ironing_celsius, linewidth = 1, alpha = 0.7, label = "Ironing Room")
+        ax1.set_xlabel('Nov 1st 2016 - May 27th 2016'); 
+        ax1.legend()
+    
+        # HUMIDITY Outside vs HUMIDITY Wheather Station
+    
+        # Outiside
+        ax2.plot(df.date, df.portico_hum_perc, linewidth = 1, alpha = 0.7, label = "North Outside House")
+        ax2.set_xlabel('Nov 1st 2016 - May 27th 2016'); ax2.set_ylabel('Humidity in %'); ax2.set_title('Humidity Trend')
+        # Weather Station
+        ax2.plot(df.date, df.cws_hum_perc, linewidth = 1, alpha = 0.7, label = "Weather Station")
+        ax2.set_xlabel('Nov 1st 2016 - May 27th 2016')
+        ax2.legend()
+    
+        # OUTSIDE HUMIDITY vs VISIBILITY 
+    
+        # Outside Humidity
+        ax3.plot(df.date, df.cws_hum_perc, linewidth = 1, alpha = 0.7, label = "Outside Humidity")
+        ax3.set_xlabel('Nov 1st 2016 - May 27th 2016'), ax3.set_title('Humidity vs Visibility')
+        # Visibility
+        ax3.plot(df.date, df.cws_visibility, linewidth = 1, alpha = 0.7, label = "Visibility")
+        ax3.set_xlabel('Nov 1st 2016 - May 27th 2016')
+        ax3.legend()
+    
+        # TEMPERATURE Outside vs TEMPERATURE Wheather Station
+        # Outiside
+        ax4.plot(df.date, df.portico_celsius, linewidth = 1, alpha = 0.7, label = "North Outside House")
+        ax4.set_xlabel('Nov 1st 2016 - May 27th 2016'); ax2.set_ylabel('Temp in Celsius'); ax4.set_title('Temperature Trend')
+        # Weather Station
+        ax4.plot(df.date, df.cws_celsius, linewidth = 1, alpha = 0.7, label = "Weather Station")
+        ax4.set_xlabel('Nov 1st 2016 - May 27th 2016')
+        ax4.legend()
+
+        plt.tight_layout(pad=2)
+        plt.savefig("Relations.jpg", pdi = 300)
+        
+        
+    def plot_2(self, df, m1, m2, m3):
+        self.df = df
+        
+        size = int(len(df)*0.75)
+        fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(nrows = 3, ncols = 2, figsize = (35, 15))
+        space = np.linspace(0, 100)
+        
+        ax1.plot(space, m1.coef_[0][4]*space + m1.intercept_); ax1.set_title('Laundry °C')
+        ax1.scatter(df.laundry_celsius[size:], df.appliance_wh[size:])
+
+        ax2.plot(space, m1.coef_[0][1]*space + m1.intercept_); ax2.set_title('Kitchen Humidity %')
+        ax2.scatter(df.kitchen_hum_perc[size:], df.appliance_wh[size:])
+
+        ax3.plot(space, m2.coef_[0][3]*space + m2.intercept_); ax3.set_title('Laundry °C LASSO')
+        ax3.scatter(df.laundry_celsius[size:], df.appliance_wh[size:])
+
+        ax4.plot(space, m2.coef_[0][1]*space + m2.intercept_); ax4.set_title('Kitchen Humidity % LASSO')
+        ax4.scatter(df.kitchen_hum_perc[size:], df.appliance_wh[size:])
+
+        ax5.plot(space, m3.coef_[0][4]*space + m3.intercept_); ax5.set_title('Laundry °C RFECV')
+        ax5.scatter(df.laundry_celsius[size:], df.appliance_wh[size:])
+
+        ax6.plot(space, m3.coef_[0][1]*space + m3.intercept_); ax6.set_title('Kitchen Humidity % RFECV')
+        ax6.scatter(df.kitchen_hum_perc[size:], df.appliance_wh[size:])
+        plt.savefig("linear_model.jpg", dpi = 300)
---
+++ decision_tree_opt.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, mean_absolute_error, mean_squared_error
+import lightgbm as lgb
+
+class Parameters:
+    
+    def evaluate(self, true, predict):
+        self.model = true
+        self.features = predict
+        
+        print('Model Performance')
+        print("MAE:", round(mean_absolute_error(true, predict), 5) )
+        print("MSE:", round(mean_squared_error(true, predict), 5) )
+        print("Variance:", round(np.var(predict), 5) )
+        print("Accuracy:", round(    100 - np.mean(abs((predict - true)/true)*100)      , 2), "%" )
+        print()
+        return None
+    
+    def dtr_importance(self, df, model):
+        self.df = df
+        self.model = model
+        
+        imp = dict(zip(df.drop("appliance_wh", axis = 1).columns, list(model.feature_importances_)))
+        imp = pd.DataFrame(imp, index = range(len(list(model.feature_importances_))))
+        print(imp.loc[25:].T.sort_values(25, ascending = False).rename(columns = {25:"values"}).head() )
+        print()
+        return None
+        
+    def dict_tuning(self, train):
+        self.train = train
+        ccp_alpha = list(range(100))        # 1
+        criterion = ["mse", "friedman_mse", "mae"]        # "mse"
+        max_depth = list(range(1,50)) # 2
+        max_features = ["auto", "sqrt", "log2"] + list(range(1,train.shape[1]))        # 1
+        max_leaf_nodes = list(range(2,50)) # 3
+        min_impurity_decrease = np.arange(0.0,1.0, 0.001)        # 0.011
+        min_impurity_split = np.arange(0.0,1.0, 0.001)        # 0.485
+        min_samples_leaf = np.arange(0,1,0.1)        # 0.2
+        min_samples_split = list(range(2,50))        # 49
+        min_weight_fraction_leaf = np.arange(0,1,0.1)        # 0.2
+        random_state = list(range(0,1000))        # 529
+        splitter = ["best", "random"]        # "random"
+
+        random_grid = { "ccp_alpha": ccp_alpha,
+                        "criterion": criterion,
+                        "max_depth": max_depth,
+                        "max_features": max_features,
+                        "max_leaf_nodes": max_leaf_nodes,
+                        "min_impurity_decrease": min_impurity_decrease,
+                        "min_impurity_split": min_impurity_split,
+                        "min_samples_leaf": min_samples_leaf,
+                        "min_samples_split": min_samples_split,
+                        "min_weight_fraction_leaf": min_weight_fraction_leaf,
+                        "random_state": random_state,
+                        "splitter": splitter }
+        """
+        The tuning of the parameters is carried out through the GridSearchCV. In the notebook all parameters shown come from
+        the results from GridSearchCV.
+        
+        tree_random = GridSearchCV(decisiontree, grid, n_jobs = -1, cv = 2)
+        tree_random.fit(X_train, y_train_tree)
+        tree_model = tree_random.best_estimator_
+        print (tree_random.best_score_, tree_random.best_params_)
+        """
+        return random_grid
